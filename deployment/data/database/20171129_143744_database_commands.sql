aa298ab1bc6df100e4d2fbc4c6536ea1744f420c
Tim van Steenbergen <tim@tieka.nl>
2017-12-12 15:57:31 +0100

    only the essentials

--- deployment/data/database/20171129_143744_database_commands.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE `zipcodehousenumber$landen` RENAME TO `zipcodehousenumber$what`;
-UPDATE `mendixsystem$entity`
- SET `entity_name` = 'ZipcodeHousenumber.What', 
-`table_name` = 'zipcodehousenumber$what', 
-`superentity_id` = NULL
- WHERE `id` = 'f248f266-b10f-41aa-badb-f3b4ed194a6f';
-UPDATE `mendixsystem$version`
- SET `versionnumber` = '4.2', 
-`lastsyncdate` = '20171129 14:35:20';
--- deployment/web/widgets/widgets.js
+++
@@ -1,1 +0,0 @@
-define(["GeoLocationWidget/widget/GeoLocationWidget", "MobileFeatures/widget/MobileFeatures"], {});
